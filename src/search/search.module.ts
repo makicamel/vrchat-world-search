0009e4203d71fd6b48da996b9bf53d47c72faf66
makicamel <unright@gmail.com>
2022-03-26 19:30:23 +0900

    Get config through ConfigService

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -5,8 +5,16 @@ import { SearchService } from './search.service';
 
 @Module({
   imports: [
-    ElasticsearchModule.register({
-      node: 'http://localhost:9200',
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get('ELASTICSEARCH_NODE'),
+        auth: {
+          username: configService.get('ELASTICSEARCH_USERNAME'),
+          password: configService.get('ELASTICSEARCH_PASSWORD'),
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [SearchService],
