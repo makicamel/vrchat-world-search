7e3e12d1a0c8efefa45c4196092483d78faa74d8
makicamel <unright@gmail.com>
2022-03-27 07:46:29 +0900

    nest generate controller service module worlds

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchModule } from './search/search.module';
+import { WorldsModule } from './worlds/worlds.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SearchModule],
+  imports: [ConfigModule.forRoot(), SearchModule, WorldsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
