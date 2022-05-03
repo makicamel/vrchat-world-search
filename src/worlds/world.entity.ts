4f5ad6150743265af5f27dab657148c93d0f23f6
makicamel <unright@gmail.com>
2022-05-03 15:30:20 +0900

    We need just a class
    We don't use relational database

--- src/worlds/world.entity.ts
+++ src/worlds/world.entity.ts
@@ -1,23 +1,10 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm';
 import { WorldInterface } from './types/world.interface';
 
-@Entity()
 export class World implements WorldInterface {
-  @PrimaryColumn()
   id: string;
-
-  @Column()
   worldName: string;
-
-  @Column()
   authorId: string;
-
-  @Column()
   authorName: string;
-
-  @Column()
   imageUrl: string;
-
-  @Column()
   thumbnailImageUrl: string;
 }
