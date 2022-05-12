74e8d8a9a42583a715b73338ebab332b6b247c47
makicamel <unright@gmail.com>
2022-05-13 08:49:38 +0900

    Create Theme

---
+++ frontend/src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    divider: '#fff',
+    background: {
+      default: '#1b2025',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#fff',
+    },
+  }
+})
+
+
+export default theme;
