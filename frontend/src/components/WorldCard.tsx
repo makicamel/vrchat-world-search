ae1cdf68e9442097dd99ec7028ab22d9d99af7a3
makicamel <unright@gmail.com>
2022-05-22 20:16:54 +0900

    Add link to VRChat.com

--- frontend/src/components/WorldCard.tsx
+++ frontend/src/components/WorldCard.tsx
@@ -1,7 +1,9 @@
+import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Chip from '@mui/material/Chip'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import { CardActionArea } from '@mui/material'
@@ -40,6 +42,18 @@ const WorldCard = (props: { world: World }) => (
             <Chip label={tag} size="small" clickable={true} color="info" className={styles.chip} />
           ))}
         </div>
+        <Button
+          variant="contained"
+          color="info"
+          startIcon={<OpenInNewIcon />}
+          disableElevation={true}
+          href={`https://vrchat.com/home/world/${props.world.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: '#6ae3f9', textTransform: 'none' }}
+        >
+          VRChat.com
+        </Button>
       </CardContent>
     </CardActionArea>
   </Card>
