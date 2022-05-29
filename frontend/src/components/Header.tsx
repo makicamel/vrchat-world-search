77e49a77dbbd22a7fa1c281664f54e9121cf440b
makicamel <unright@gmail.com>
2022-05-29 10:34:32 +0900

    Apply styles through styled-components

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,30 @@
 import { Dispatch, SetStateAction } from 'react'
-import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+
+const HeaderElement = styled.header`
+  background-color: #07242b;
+  border-bottom: 3px solid #053c48;
+  font-family: Dosis, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif;
+  font-size: 3rem;
+  margin: 0;
+  padding: 0 2rem;
+`
+
+const TitleElement = styled.span`
+&:hover {
+  cursor: pointer;
+}
+`
 
 const Header: React.FC<{
   setAuthorId: Dispatch<SetStateAction<string | undefined>>
 }>
   = ({ setAuthorId }): JSX.Element => (
-    <header className={styles.header} onClick={() => setAuthorId(undefined)}>
-      VRChat World Search
-    </header>
+    <HeaderElement>
+      <TitleElement onClick={() => setAuthorId(undefined)}>
+        VRChat World Search
+      </TitleElement>
+    </HeaderElement>
   )
 
 export default Header
