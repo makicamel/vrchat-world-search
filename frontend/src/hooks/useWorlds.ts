eadce1c60c6f836f5909568fdc4ca1578e518419
makicamel <unright@gmail.com>
2022-06-04 14:54:56 +0900

    loadMoreWorlds only when not loading more and not reach end

--- frontend/src/hooks/useWorlds.ts
+++ frontend/src/hooks/useWorlds.ts
@@ -3,6 +3,8 @@ import axios, { AxiosError } from 'axios'
 import useSWRInfinite from 'swr/infinite'
 import { WorldInterface as World } from '../../types/world.interface'
 
+const PAGE_SIZE = 10
+
 const fetcher = (url: string) => {
   const apiClient = axios.create({
     baseURL: 'http://localhost:3001',
@@ -16,14 +18,17 @@ const useWorldsWithAuthorId = () => {
   const [authorId, setAuthorId] = useState<string | undefined>(undefined);
   const query = (authorId) ? `&authorId=${authorId}` : ''
 
-  const { data, error, isValidating, size, setSize } = useSWRInfinite(
+  const { data, error, size, setSize } = useSWRInfinite(
     (index: number) => `/worlds?page=${index}${query}`,
     (url: string) => fetcher(url)
   )
   const worlds: World[] | undefined = data ? data.flat() : undefined
+  const isLoadingMore = (size > 0 && data && typeof data[size - 1] === 'undefined')
+  const isEmpty = data?.[0]?.length === 0
+  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE)
 
   const loadMoreWorlds = () => {
-    if (!isValidating) setSize(size + 1)
+    if (!isLoadingMore && !isReachingEnd) setSize(size + 1)
   }
 
   return { worlds, error, authorId, setAuthorId, loadMoreWorlds }
