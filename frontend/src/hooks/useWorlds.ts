4fd855df13092e097dba479558f2de4c06dfd97a
makicamel <unright@gmail.com>
2022-06-04 14:27:04 +0900

    Use key with cache

--- frontend/src/hooks/useWorlds.ts
+++ frontend/src/hooks/useWorlds.ts
@@ -3,36 +3,30 @@ import axios, { AxiosError } from 'axios'
 import useSWRInfinite from 'swr/infinite'
 import { WorldInterface as World } from '../../types/world.interface'
 
-const fetcher = (url: string, pageIndex: number, authorId?: string) => {
+const fetcher = (url: string) => {
   const apiClient = axios.create({
     baseURL: 'http://localhost:3001',
     timeout: 5000,
     headers: { 'Content-Type': 'application/json' }
   });
-  const query = (authorId) ? `&authorId=${authorId}` : ''
-  return apiClient.get(`${url}?page=${pageIndex}${query}`).then((response) => response.data)
+  return apiClient.get(url).then((response) => response.data)
 }
 
 const useWorldsWithAuthorId = (initialAuthorId?: string) => {
   const [authorId, setAuthorId] = useState(initialAuthorId);
-  const { data, error, size, setSize } = useSWRInfinite(
-    getKey,
-    (url: string, index: number) => fetcher(url, index, authorId)
+  const query = (authorId) ? `&authorId=${authorId}` : ''
+
+  const { data, error, isValidating, size, setSize } = useSWRInfinite(
+    (index: number) => `/worlds?page=${index}${query}`,
+    (url: string) => fetcher(url)
   )
   const worlds: World[] | undefined = data ? data.flat() : undefined
 
   const loadMoreWorlds = () => {
-    setTimeout(() => {
-      setSize(size + 1)
-    }, 250)
+    if (!isValidating) setSize(size + 1)
   }
 
   return { worlds, error, authorId, setAuthorId, loadMoreWorlds }
-}
-
-const getKey = (pageIndex: number, previousPageData: any[]) => {
-  if (previousPageData && !previousPageData.length) return null
-  return ['/worlds', pageIndex]
 }
 
 export default useWorldsWithAuthorId;
