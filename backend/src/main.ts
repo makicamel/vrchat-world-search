45f38262c14f45ad29002beb38734845583fd968
makicamel <unright@gmail.com>
2022-08-22 08:08:18 +0900

    Use environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
-  await app.listen(3001);
+  const config = app.get(ConfigService)
+  app.enableCors({ origin: config.get('FRONTEND_URL') });
+  await app.listen(config.get('BACKEND_PORT'));
 }
 bootstrap();
